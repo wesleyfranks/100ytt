f647665f2f8b3a7fbbddfdc7e6212af3c42601a5
Wesley Franks <code.wesleyfranks@gmail.com>
2025-10-21 09:30:31 -0500

    Update YouTubeProfile.tsx

--- src/components/YouTubeProfile.tsx
+++ src/components/YouTubeProfile.tsx
@@ -4,7 +4,7 @@ const YouTubeProfile = () => {
   return (
     <div className="flex items-center space-x-4 mb-8 mt-8">
       <img
-        src="https://yt3.googleusercontent.com/8ed6g9LRSWJ2A0MOLBRTTExBVc6NCX3kB80Cj0LBdjAMM63KqFikYgfDld8pQ8eNnZ-zIw0L3g=s160-c-k-c0x00ffffff-no-rj"
+        src="https://yt3.googleusercontent.com/Uqy3a1GKRQAhGnovbnwiGdWc5SJXyTwFQmiQByqcC5iSWoDW8Sze7UbqCwHc9lwb45_fXKCeCQE=s160-c-k-c0x00ffffff-no-rj"
         alt="Wesley"
         className="w-16 h-16 rounded-full"
       />
